bfbe384112d4b7ba5eaafece759f5fae4c427c8e
shailendracss <shailendra.rajawat@cssindiaonline.com>
2020-07-25 16:22:42 +0530

    Modified Git Ignore file

--- webdriverDemo/MultiBrowser.py
+++ webdriverDemo/MultiBrowser.py
@@ -9,7 +9,7 @@ FFPATH = os.getcwd()+"/../DriverBinaries/geckodriver"
 driver = webdriver.Chrome(executable_path=CHROMEPATH)
 #driver = webdriver.Firefox(executable_path=FFPATH)
 
-path = driver.get_screenshot_as_png();
+path = driver.get_screenshot_as_png()
 
 
 
